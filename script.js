8ba9d9bdb8136f348e7df2e552c75c0bceb74ef1
Arauco <arishel@gmail.com>
2019-12-13 09:27:36 -0800

    added UV index

--- script.js
+++ script.js
@@ -42,13 +42,20 @@ $(".cityBtn").click(function () {
 ////////// Fetch Data from server /////////////////////
 // Here we run our AJAX call to the OpenWeatherMap API
 function getCity() {
+	var lat;
+	var lon;
+
 	var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&appid=" + APIKey;
 
+	// get 5 day weather
 	$.ajax({
 		url: queryURL,
 		method: "GET",
-		// dataType: "json",
 		success: function (data) {
+			lat = data.city.coord.lat
+			lon = data.city.coord.lon
+			console.log("lat: " + lat + ", lon: " + lon);
+
 
 			// Update current city
 			currentCity = data.city.name;
@@ -65,7 +72,6 @@ function getCity() {
 				displayCity()
 
 			} else {
-
 				// Create button
 				var cityBtn = $("<button>");
 				cityBtn.addClass("list-group-item list-group-item-action cityBtn");
@@ -81,35 +87,48 @@ function getCity() {
 
 			/////////////// Display Current Weather /////////////////
 			function displayCity() {
-				for (let i = 0; i < data.list.length/8; i++) {
+				for (let i = 0; i < data.list.length / 8; i++) {
 					var d = 0;
-														
-					$(".currentCity"+ (i+1)).html(data.city.name + " Weather Details");
-					$(".wind"+ (i+1)).text("Wind Speed: " + data.list[d].wind.speed);
-					$(".humidity"+ (i+1)).text("Humidity: " + data.list[d].main.humidity);
-					$(".temp"+ (i+1)).text("Temperature: " + (((data.list[d].main.temp - 273.15) * 1.80) + 32).toFixed() + " F");
-					$(".uvIndex"+ (i+1)).text("UV Index: " + data.list[d].main.temp);
-					d=d+8
+
+					$(".currentCity" + (i + 1)).html(data.city.name + " Weather Details");
+					$(".wind" + (i + 1)).text("Wind Speed: " + data.list[d].wind.speed);
+					$(".humidity" + (i + 1)).text("Humidity: " + data.list[d].main.humidity);
+					$(".temp" + (i + 1)).text("Temperature: " + (((data.list[d].main.temp - 273.15) * 1.80) + 32).toFixed() + " F");
+					d = d + 8
 				}
 
+				// Previous loup only returns 4 days. This runs to get the last data point
 				$(".currentCity6").html(data.city.name + " Weather Details");
-					$(".wind6").text("Wind Speed: " + data.list[39].wind.speed);
-					$(".humidity6").text("Humidity: " + data.list[39].main.humidity);
-					$(".temp6").text("Temperature: " + (((data.list[39].main.temp - 273.15) * 1.80) + 32).toFixed() + " F");
-					$(".uvIndex6").text("UV Index: " + data.list[39].main.temp);
-					d=d+8
+				$(".wind6").text("Wind Speed: " + data.list[39].wind.speed);
+				$(".humidity6").text("Humidity: " + data.list[39].main.humidity);
+				$(".temp6").text("Temperature: " + (((data.list[39].main.temp - 273.15) * 1.80) + 32).toFixed() + " F");
+				d = d + 8
+				getUV(lat, lon);
 			}
+		}
+	})
+}
+
+// Get UV index
+function getUV(lat, lon) {
+
+	var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon
+	$.ajax({
+		url: queryURL2,
+		method: "GET",
+		success: function (data2) {
+			console.log(data2);
+			$(".uvIndex").text("UV Index: " + data2.value);
 		}
 	})
 }
 
 
 // if (localStorage.getItem(searchHist) !== null) {
-	
-	var storeHist = JSON.parse(localStorage.getItem("storeHist"))
-	for (let i = 0; i < storeHist.length; i++) {
-		currentCity = storeHist[i]
-		getCity();
-	}
-	currentCity = storeHist[0]
-	
+
+var storeHist = JSON.parse(localStorage.getItem("storeHist"))
+for (let i = 0; i < storeHist.length; i++) {
+	currentCity = storeHist[i]
+	getCity();
+}
+currentCity = storeHist[0]
